7c6bb8a55aa0207c58399be3dcd16588241c8072
hcc391 <143318069+hcc391@users.noreply.github.com>
2024-03-05 18:15:37 +0800

    Update install-openssl_3.sh

--- install-openssl_3.sh
+++ install-openssl_3.sh
@@ -87,7 +87,3 @@ mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 cp ~/openssl/openssl3.spec /root/rpmbuild/SPECS/openssl.spec
 
 mv openssl-3.2.1.tar.gz /root/rpmbuild/SOURCES
-cd /root/rpmbuild/SPECS && \
-    rpmbuild \
-    -D "version 3.2.1" \
-    -ba openssl.spec
